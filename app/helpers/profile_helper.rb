5b5cc215825ff9dafd676107d215f536d4b4e77d
Josh Kierpiec <jkierpiec@solidyn.com>
2013-01-21 20:10:29 -0700

    some reason .gitignore had the yml file in there

---
+++ app/helpers/profile_helper.rb
@@ -0,0 +1,26 @@
+module ProfileHelper
+    def height_ft
+        [
+            %w(4),
+            %w(5),
+            %w(6),
+            %w(7)
+        ]
+    end
+
+    def height_in
+        [
+            %w(1),
+            %w(2),
+            %w(3),
+            %w(4),
+            %w(5),
+            %w(6),
+            %w(7),
+            %w(8),
+            %w(9),
+            %w(10),
+            %w(11)
+        ]
+    end
+end
